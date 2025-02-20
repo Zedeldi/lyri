e285d094bcb8c61834a65aac1507f893c0dcb5bd
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2025-02-20 14:35:27 +0000

    Add command-line argument to open web browser

--- lyri/__main__.py
+++ lyri/__main__.py
@@ -1,5 +1,6 @@
 """Entry-point to start Lyri web server."""
 
+import webbrowser
 from argparse import ArgumentParser
 
 from sanic import Sanic
@@ -11,11 +12,12 @@ def get_parser() -> ArgumentParser:
     parser = ArgumentParser(
         prog="Lyri",
         description="Start Lyri web server",
-        epilog="Copyright (C) 2024 Zack Didcott",
+        epilog="Copyright (C) 2025 Zack Didcott",
     )
     parser.add_argument("--host", type=str, default="127.0.0.1", help="Host address")
     parser.add_argument("--port", type=int, default=8080, help="Port to listen on")
     parser.add_argument("--dev", action="store_true", help="Enable development mode")
+    parser.add_argument("--browser", "-b", action="store_true", help="Open web browser")
     return parser
 
 
@@ -26,6 +28,8 @@ def main() -> None:
     loader = AppLoader(module_input="lyri.server.app")
     app = loader.load()
     app.prepare(host=args.host, port=args.port, dev=args.dev)
+    if args.browser:
+        webbrowser.open_new(f"http://{args.host}:{args.port}")
     Sanic.serve(primary=app, app_loader=loader)
 
 
